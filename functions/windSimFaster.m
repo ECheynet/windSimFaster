cbe654d13498f4fa04ddd57b7215873ff5d5f107
E.  Cheynet <ECheynet@users.noreply.github.com>
2025-11-18 14:42:49 +0100

    Add files via upload

--- functions/windSimFaster.m
+++ functions/windSimFaster.m
@@ -1,4 +1,4 @@
-function [u_corr] = windSimFaster(Y,Z,U,Cy,Cz,f,u)
+function [u_corr] = windSimFaster(Y,Z,U,Cy,Cz,f,u,varargin)
 % windSimFaster Introduces spatial correlation into uncorrelated wind 
 %velocity histories using Davenport's coherence model.
 %
@@ -49,20 +49,56 @@ function [u_corr] = windSimFaster(Y,Z,U,Cy,Cz,f,u)
 % 
 % See also FFT, IFFT, CHOL, LDL.
 
+%% input parser
+p = inputParser();
+p.CaseSensitive = false;
+p.addOptional('cohmodel','Davenport');
+p.parse(varargin{:});
+cohmodel = p.Results.cohmodel;
 %% Prepare data for the coherence
 % [Nzz,Nyy]=size(Y);
 dy = abs(Y(:)'-Y(:)); % Matrix distance along y
 dz = abs(Z(:)'-Z(:)); % Matrix distance along z
+z_avg = 0.5*(Z(:)'+Z(:));
 meanU = 0.5*(U(:)'+U(:)); % Mean wind velocity between each nodes
-% Davenport decay coefficient Cy(1x1) with lateral separation
-ay = Cy.*dy;
-% Davenport decay coefficient Cz (1x1) with  vertical separation
-az = Cz.*dz;
-% Combine them into the coherence matrix for lateral and vertical
-% separations
-K = -sqrt(ay.^2+az.^2)./meanU;
+
 % Anonymous function
-cohDavenport = @(K,f) exp(K.*f);
+if strcmpi(cohmodel,'Davenport')
+    
+    % Davenport decay coefficient Cy(1x1) with lateral separation
+    ay = Cy(1).*dy;
+    % Davenport decay coefficient Cz (1x1) with  vertical separation
+    az = Cz(1).*dz;
+    % Combine them into the coherence matrix for lateral and vertical
+    % separations
+    K = -sqrt(ay.^2+az.^2)./meanU;
+
+    modelFunCoh = @(K,f) exp(K.*f);
+    
+elseif strcmpi(cohmodel,'Bowen')
+
+    % Bowen coefficient with lateral separation
+    ay = Cy(1).*dy + Cy(2).*dy.^2./z_avg;
+    % Bowen coefficient with lateral separation
+    az =Cz(1).*dz + Cz(2).*dz.^2./z_avg;
+
+    % Combine them into the coherence matrix for lateral and vertical separations
+    K = -sqrt(ay.^2+az.^2)./meanU;
+
+    modelFunCoh = @(K,f) exp(K.*f);
+
+
+elseif strcmpi(cohmodel,'Bowen2')
+
+    [K] = coh_bowen2(Cy,Cz,f,meanU,dy,dz,z_avg);
+
+    modelFunCoh = @(K,f) exp(K.*f);
+else
+    error('unknown coherence model')
+end
+
+
+
 
 %% Introduce coherence
 % Define a frequency vector
@@ -70,28 +106,35 @@ f2s = [0 f(1:end-1)];
 f2s = [f2s fliplr(f2s)];% 2-sided frequency array
 tic
 N = numel(f2s);
-
 fftU = (fft(u));
 for ii=2:N
-    cohU = cohDavenport(K,f2s(ii));
-    if min(cohU(:))<0.98
-        try
-            C = chol(cohU,'lower');
-        catch exception
-            [L,D]=ldl(cohU,'lower'); % a LDL decomposition is applied this time
-            C = (L*sqrt(D));
-        end
-        fftU(ii,:)= C*exp(1i*angle(fftU(ii,:)')).*abs(fftU(ii,:)');
-    else
-        fftU(ii,:)= mean(fftU(ii,:));
+    cohU = modelFunCoh(K,f2s(ii));
+    try
+        C = chol(cohU,'lower');
+    catch exception
+        [L,D]=ldl(cohU,'lower'); % a LDL decomposition is applied this time
+        C = (L*sqrt(D));
     end
+    fftU(ii,:)= C*exp(1i*angle(fftU(ii,:)'));
 end
+fftU = fftU.*abs(fft(u));
 toc
 % zero mean value
 fftU(1,:)=0;
 % Get back to the time domain
 u_corr = real(ifft(fftU));
-% % Add the mean wind speed
-% u_corr = (u_corr'+ U(:))';
+%% Nested function
+
+    function [K_Bowen2] = coh_bowen2(Cy,Cz,f,meanU,dy,dz,Z)
+
+
+     % Modified Bowen coefficient with lateral separation
+    ay_Bowen2 = sqrt((Cy(1).*f).^2 + (Cy(3)).^2).*dy + Cy(2).*dy^2./Z.*f;
+    % Bowen coefficient with lateral separation
+    az_Bowen2 = sqrt((Cz(1).*f).^2 + (Cz(3)).^2).*dz + Cz(2).*dz^2./Z.*f;
+    % Combine them into the coherence matrix for lateral and vertical separations
+    K_Bowen2 = -sqrt(ay_Bowen2.^2+az_Bowen2.^2)./(f.*meanU);    
+end
+
 
 end
