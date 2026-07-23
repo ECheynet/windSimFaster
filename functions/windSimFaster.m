fe89a5218c9f7a917d39b90b4c45f03060df2cfe
E.  Cheynet <ECheynet@users.noreply.github.com>
2026-07-23 11:35:11 +0200

    Refactor windSimFaster with new coherence models
    Updated windSimFaster function to include additional coherence models and improved handling of frequency vectors. Enhanced input parsing and added comments for clarity.

--- functions/windSimFaster.m
+++ functions/windSimFaster.m
@@ -1,11 +1,11 @@
 function [u_corr] = windSimFaster(Y,Z,U,Cy,Cz,f,u,varargin)
-% windSimFaster Introduces spatial correlation into uncorrelated wind 
+% windSimFaster Introduces spatial correlation into uncorrelated wind
 %velocity histories using Davenport's coherence model.
 %
 %   This function simulates correlated wind fields over a grid defined by Y
-%   and Z coordinates, based on mean wind speeds U, Davenport coherence 
-%   coefficients Cy and Cz, frequency vector f, and an array of uncorrelated 
-%   wind velocities u. 
+%   and Z coordinates, based on mean wind speeds U, Davenport coherence
+%   coefficients Cy and Cz, frequency vector f, and an array of uncorrelated
+%   wind velocities u.
 %
 % Syntax:
 %   [u_corr] = windSimFaster(Y, Z, U, Cy, Cz, f, u)
@@ -13,11 +13,11 @@ function [u_corr] = windSimFaster(Y,Z,U,Cy,Cz,f,u,varargin)
 % Inputs:
 %   Y   - Matrix of y-coordinates (lateral) of grid nodes. [Nzz,Nyy] = size(Y).
 %   Z   - Matrix of z-coordinates (vertical) of grid nodes. Size must match Y.
-%   U   - Vector or matrix of mean wind speed at each grid node. If a vector, 
+%   U   - Vector or matrix of mean wind speed at each grid node. If a vector,
 %         it must have one element for each node.
 %   Cy  - Scalar Davenport coherence coefficient for lateral (y-direction)
 %         separations.
-%   Cz  - Scalar Davenport coherence coefficient for vertical (z-direction) 
+%   Cz  - Scalar Davenport coherence coefficient for vertical (z-direction)
 %         separations.
 %   f   - Vector of frequency steps for the simulation.
 %   u   - Matrix of uncorrelated wind velocities. Each column corresponds to
@@ -45,8 +45,8 @@ function [u_corr] = windSimFaster(Y,Z,U,Cy,Cz,f,u,varargin)
 % % Simulate spatially correlated wind velocities
 % u_corr = windSimFaster(Y, Z, U, Cy, Cz, f, u);
 %
-% Author: E. Cheynet - uiB - Last modified 2024-04-16
-% 
+% Author: E. Cheynet - uiB - Last modified 2026-07-23
+%
 % See also FFT, IFFT, CHOL, LDL.
 
 %% input parser
@@ -64,7 +64,7 @@ meanU = 0.5*(U(:)'+U(:)); % Mean wind velocity between each nodes
 
 % Anonymous function
 if strcmpi(cohmodel,'Davenport')
-    
+
     % Davenport decay coefficient Cy(1x1) with lateral separation
     ay = Cy(1).*dy;
     % Davenport decay coefficient Cz (1x1) with  vertical separation
@@ -74,7 +74,7 @@ if strcmpi(cohmodel,'Davenport')
     K = -sqrt(ay.^2+az.^2)./meanU;
 
     modelFunCoh = @(K,f) exp(K.*f);
-    
+
 elseif strcmpi(cohmodel,'Bowen')
 
     % Bowen coefficient with lateral separation
@@ -89,10 +89,27 @@ elseif strcmpi(cohmodel,'Bowen')
 
 
 elseif strcmpi(cohmodel,'Bowen2')
-
     [K] = coh_bowen2(Cy,Cz,f,meanU,dy,dz,z_avg);
-
     modelFunCoh = @(K,f) exp(K.*f);
+
+elseif strcmpi(cohmodel,'vogt')
+
+    % Mod. Bowen coefficient with lateral separation
+    ay = dy.* Cy(1).* exp(Cy(2).*dy./z_avg);
+    % Mod. Bowen coefficient with vertical separation
+    az = dz.* Cz(1).* exp(Cz(2).*dz./z_avg);
+
+    % Combine them into the coherence matrix for lateral and vertical separations
+    M = numel(dy(:,1));
+    K = nan(M,M,2);
+    K(:,:,1) = -sqrt(ay.^2+az.^2)./meanU;
+
+    dummy1 = Cy(3).^2*dy.^2;
+    dummy2 = Cz(3).^2*dz.^2;
+    K(:,:,2) = -1./z_avg.*sqrt(dummy1 + dummy2);
+
+    modelFunCoh = @(K,f) exp(K(:,:,1).*f +K(:,:,2).*ones(size(f)));
+elseif strcmpi(cohmodel,'IEC')
 else
     error('unknown coherence model')
 end
@@ -102,39 +119,41 @@ end
 
 %% Introduce coherence
 % Define a frequency vector
-f2s = [0 f(1:end-1)];
-f2s = [f2s fliplr(f2s)];% 2-sided frequency array
 tic
-N = numel(f2s);
-fftU = (fft(u));
-for ii=2:N
+N = size(u,1);
+df = median(diff(f));
+k = 0:N-1;
+f2s = min(k,N-k)*df;
+fftU0 = fft(u);
+fftU = fftU0;
+for ii = 2:N/2
     cohU = modelFunCoh(K,f2s(ii));
-    try
-        C = chol(cohU,'lower');
-    catch exception
-        [L,D]=ldl(cohU,'lower'); % a LDL decomposition is applied this time
-        C = (L*sqrt(D));
-    end
-    fftU(ii,:)= C*exp(1i*angle(fftU(ii,:)'));
+    C = chol(cohU,'lower');
+
+    dummy = C*exp(1i*angle(fftU0(ii,:)'));
+    fftU(ii,:) = abs(fftU0(ii,:)).*exp(1i*angle(dummy.'));
+    fftU(N-ii+2,:) = conj(fftU(ii,:));
 end
-fftU = fftU.*abs(fft(u));
-toc
-% zero mean value
-fftU(1,:)=0;
-% Get back to the time domain
-u_corr = real(ifft(fftU));
+fftU(1,:) = 0;
+fftU(N/2+1,:) = real(fftU0(N/2+1,:));
+u_corr = ifft(fftU,'symmetric');
+
+
+% % Add the mean wind speed
+% u_corr = (u_corr'+ U(:))';
+
+
+
 %% Nested function
 
     function [K_Bowen2] = coh_bowen2(Cy,Cz,f,meanU,dy,dz,Z)
 
 
-     % Modified Bowen coefficient with lateral separation
-    ay_Bowen2 = sqrt((Cy(1).*f).^2 + (Cy(3)).^2).*dy + Cy(2).*dy^2./Z.*f;
-    % Bowen coefficient with lateral separation
-    az_Bowen2 = sqrt((Cz(1).*f).^2 + (Cz(3)).^2).*dz + Cz(2).*dz^2./Z.*f;
-    % Combine them into the coherence matrix for lateral and vertical separations
-    K_Bowen2 = -sqrt(ay_Bowen2.^2+az_Bowen2.^2)./(f.*meanU);    
-end
-
-
+        % Modified Bowen coefficient with lateral separation
+        ay_Bowen2 = sqrt((Cy(1).*f).^2 + (Cy(3)).^2).*dy + Cy(2).*dy^2./Z.*f;
+        % Bowen coefficient with lateral separation
+        az_Bowen2 = sqrt((Cz(1).*f).^2 + (Cz(3)).^2).*dz + Cz(2).*dz^2./Z.*f;
+        % Combine them into the coherence matrix for lateral and vertical separations
+        K_Bowen2 = -sqrt(ay_Bowen2.^2+az_Bowen2.^2)./(f.*meanU);
+    end
 end
